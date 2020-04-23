b711018c4ea3fb390e53ae7c6cb919623649dfe7
SPICA\トロ <toro970919@gmail.com>
2020-04-23 09:29:35 +0900

    add ADSL_1_1_D

--- ADSL/ADSL_1_1_D.cpp
+++ ADSL/ADSL_1_1_D.cpp
@@ -3,6 +3,19 @@
 using namespace std;  
 
 int main(){  
-    cout << "Hello" << endl;  
-    return 0;  
+    int n;
+    cin >> n;
+    int R[n];
+    for(int i=0; i<n; i++){
+        cin >> R[i];
+    }
+    int minv = R[0];
+    int maxv = pow(-10, 9);
+    for(int i=1; i<n; i++){
+        maxv = max(maxv, R[i]-minv);
+        minv = min(minv, R[i]);
+    }
+
+    cout << maxv << endl;
+    return 0;
 }
